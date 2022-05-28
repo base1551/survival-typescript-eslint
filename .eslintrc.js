e136dab2b4db553c4f023d0789988a228c550dbb
base1551 <baseball.1.kyoto@gmail.com>
2022-05-28 15:57:47 +0900

    shareable configを設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,17 @@ module.exports = {
         sourceType: "module",//JavaScriptコードがスクリプトorモジュールどちらのモードで書かれるかを指定するオプション
     },
     //以下、規約制定
+    // rules: {
+    //     "no-console": "error",//console.logを残していたらエラー
+    //     camelcase: ["error", { properties: "never" }],
+    //     semi: ["error", "always"],
+    // },
+
+    //Shareable configを導入する
+    extends: ["airbnb-base"],
+    //継承したルールから変更ßする内容を追記
     rules: {
-        "no-console": "error",//console.logを残していたらエラー
-        camelcase: ["error", { properties: "never" }],
+        "import/prefer-default-export": "off",
+        quotes: ["error", "double"],
     },
 };
